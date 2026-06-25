8e041fb107a1d75edfc4b013c7ab2db0b2487382
sleuthy-sloth <spkoehl@gmail.com>
2026-06-25 13:08:25 -0700

    f1-cli: fix install steps - build before install, correct symlink path

--- f1-cli.rb
+++ f1-cli.rb
@@ -1,5 +1,5 @@
 class F1Cli < Formula
-  desc "Formula 1 data in your terminal - schedule, results, standings, and session times"
+  desc "Formula 1 data in your terminal -- schedule, results, standings, and session times"
   homepage "https://github.com/sleuthy-sloth/f1-cli"
   url "https://github.com/sleuthy-sloth/f1-cli/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "6e5f5c4f7351a259fa89b1aef5c90d3706fafa03662d0af94537133529a43133"
@@ -8,9 +8,9 @@ class F1Cli < Formula
   depends_on "node@22"
 
   def install
-    system "npm", "install", *std_npm_args
     system "npm", "run", "build"
-    bin.install_symlink libexec/"bin/cli.js" => "f1"
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"dist/bin/cli.js" => "f1"
   end
 
   test do
